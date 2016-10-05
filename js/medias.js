dbf4ae5904d2545c986d1bffe129a07c42566f01
castano cindy <castanocindy@castanos-MacBook-Pro.local>
2016-10-06 00:07:47 +0200

    queries again

--- js/medias.js
+++ js/medias.js
@@ -8,6 +8,15 @@ $(function () {
         typeSpeed: 30
     });
 
+        $("#inpt_search").on('focus', function () {
+        $(this).parent('label').addClass('active');
+    });
+
+    $("#inpt_search").on('blur', function () {
+        if ($(this).val().length === 0) {
+            $(this).parent('label').removeClass('active');
+        }
+    });
 
 });
 
